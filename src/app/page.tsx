df76d8e201f1123f57a11148fa6efe1dfff37a4a
Aaron Lin <aaronlin256@gmail.com>
2023-07-22 14:42:50 -0400

    Select 5 random pairs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,15 +6,11 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
         <Image
           priority
           src={logo}
           alt="Match Madness"
-          />
+        />
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
@@ -22,21 +18,18 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
+            <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded-lg">
+              Hiragana
+            </button>
+            <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded-lg">
+              Katakana
+            </button>
           </a>
         </div>
       </div>
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3">
-        <Game/>
+        <Game />
       </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,21 +1,50 @@
 
 'use client'
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import hiragana from '../../constants/hiragana.json';
+import katakana from '../../constants/katakana.json';
+
+interface MatchingPair {
+  kana: string;
+  roumaji: string;
+  type: string;
+}
 
 const Game = () => {
+  const [matchingPairs, setMatchingPairs] = useState<MatchingPair[]>([]);
+
+  const selectRandomPair = () => {
+    const matchingPair = hiragana[Math.floor(Math.random() * hiragana.length)];
+    setMatchingPairs((prevMatchingPairs) => [...prevMatchingPairs, matchingPair]);
+  }
+
+  useEffect(() => {
+    selectRandomPair();
+    selectRandomPair();
+    selectRandomPair();
+    selectRandomPair();
+    selectRandomPair();
+  }, []);
+
+
+
   return (
-    <>
-      <div className="flex justify-center">
-        <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
-          Hiragana
-        </button>
-        <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
-          Katakana
-        </button>
-
+    <div className="flex justify-between gap-4">
+      <div className="grid grid-cols-1 gap-4">
+        {matchingPairs.map((matchingPair) => (
+          <button key={matchingPair.kana} className="hover:bg-gray-200 text-black font-bold py-2 px-4 border-2 border-b-4 border-gray-300 rounded-lg">
+            {matchingPair.kana}
+          </button>
+        ))}
       </div>
-
-    </>
+      <div className="grid grid-cols-1 gap-4">
+        {matchingPairs.map((matchingPair) => (
+          <button key={matchingPair.roumaji} className="hover:bg-gray-200 text-black font-bold py-2 px-4 border-2 border-b-4 border-gray-300 rounded-lg">
+            {matchingPair.roumaji}
+          </button>
+        ))}
+      </div>
+    </div>
   )
 }
 
